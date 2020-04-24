f5373e6bd6675cf2791a83e39cd406dcf4562aef
autumncolors11 <biomimetics500tour@gmail.com>
2020-04-24 18:52:30 +0900

    2020/04/24

---
+++ abc094/arc095_b/20200424134426.py
@@ -0,0 +1,57 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+n=ii()
+arr=sorted(limii())
+
+
+a=arr[-1]
+
+def take_closest(myList, myNumber):
+    """
+    Assumes myList is sorted. Returns closest value to myNumber.
+
+    If two numbers are equally close, return the smallest number.
+    """
+    pos = bisect_left(myList, myNumber)
+    if pos == 0:
+        return myList[0]
+    if pos == len(myList):
+        return myList[-1]
+    before = myList[pos - 1]
+    after = myList[pos]
+    if after - myNumber &lt; myNumber - before:
+       return after
+    else:
+       return before
+
+print(a,take_closest(arr,a/2))
+
