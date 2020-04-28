e1789faef3576ce773321349b1a3383ac5a3ad29
autumncolors11 <biomimetics500tour@gmail.com>
2020-04-29 00:11:06 +0900

    2020/04/29

---
+++ abc094/arc095_a/20200429000606.py
@@ -0,0 +1,50 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+n=ii()
+arr0=limii()
+arr=sorted(arr0)
+
+#print(arr)
+
+cenleft=arr[n//2 -1]
+cenright=arr[n//2]
+for i in range(n):
+    if arr0[i]==cenleft:
+        print(cenright)
+    elif arr0[i]==cenright:
+        print(cenleft)
+    elif arr0[i]&gt;=cenright:
+        print(cenleft)
+    else:
+        print(cenright)
+
+
