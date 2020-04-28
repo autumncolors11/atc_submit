1cdebd45ba0fb02d120175006bcecb78e5ed17d6
autumncolors11 <biomimetics500tour@gmail.com>
2020-04-28 23:00:07 +0900

    2020/04/28

---
+++ abc003/abc003_1/20200428215625.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+n=ii()
+ans=0
+for i in range(1,n+1):
+    ans+=i*10000/n
+print(ans)
+
+
+
