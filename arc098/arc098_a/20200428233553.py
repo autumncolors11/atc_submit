5962471d8e5ed6f3fee7334ea629853d55e3cbbd
autumncolors11 <biomimetics500tour@gmail.com>
2020-04-29 00:00:06 +0900

    2020/04/29

---
+++ arc098/arc098_a/20200428233553.py
@@ -0,0 +1,51 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+n=ii()
+arr=ss()
+wcnt,ecnt=0,0
+ans=10**100
+wsum=arr.count("W")
+esum=n-wsum
+
+for i in range(n):
+    if arr[i]=="W":
+        ans=min(wcnt+esum-ecnt,ans)
+        #print(wcnt+esum-ecnt)
+
+        wcnt+=1
+    else:
+        ecnt+=1
+        ans=min(wcnt+esum-ecnt,ans)
+        #(wcnt+esum-ecnt)
+print(ans)
+
+
