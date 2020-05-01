51b5bf9058c5c4710d9a929ee94058726ed541d4
autumncolors11 <biomimetics500tour@gmail.com>
2020-05-02 02:11:36 +0900

    2020/05/02

---
+++ abc051/abc051_c/20200502020956.py
@@ -0,0 +1,49 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+sx,sy,tx,ty = mii()
+
+route=[]
+
+route.extend(["U"]*(ty-sy))
+route.extend(["R"]*(tx-sx))
+route.extend(["D"]*(ty-sy))
+route.extend(["L"]*(tx-sx))
+
+route.extend(["L"])
+route.extend(["U"]*(ty-sy+1))
+route.extend(["R"]*(tx-sx+1))
+route.extend(["D"])
+route.extend(["R"])
+route.extend(["D"]*(ty-sy+1))
+route.extend(["L"]*(tx-sx+1))
+route.extend(["U"])
+print("".join(route))
