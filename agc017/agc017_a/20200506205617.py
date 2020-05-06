f07cf40c8989e31943811ed31748641c8cf338bd
autumncolors11 <biomimetics500tour@gmail.com>
2020-05-06 20:57:56 +0900

    2020/05/06

---
+++ agc017/agc017_a/20200506205617.py
@@ -0,0 +1,62 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#https://atcoder.jp/contests/agc017/tasks/agc017_a
+from operator import mul
+from functools import reduce
+
+def cmb(n,r):
+    r = min(n-r,r)
+    if r == 0: return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1,r + 1))
+    return over // under
+
+n,p=mii()
+arr=limii()
+even=0
+odd=0
+for i in arr:
+    if i%2==0:
+        even+=1
+    else:
+        odd+=1
+
+ans=0
+
+if p==0:
+    for i in range(0,even+1):
+        for j in range(0,odd+1,2):
+            ans+=cmb(even,i)*cmb(odd,j)
+else:
+    for i in range(1,odd+1,2):
+        for j in range(0,even+1):
+            ans+=cmb(odd,i)*cmb(even,j)
+print(ans)
