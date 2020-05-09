a0137bc6d441510a9bf1bf7d0c719102a1f2a08a
autumncolors11 <biomimetics500tour@gmail.com>
2020-05-09 20:00:05 +0900

    2020/05/09

---
+++ arc004/arc004_2/20200509182719.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+n=ii()
+arr=[]
+for _ in range(n):
+    a=ii()
+    arr.append(a)
+
+arr.sort()
+
+print(sum(arr))
+
+if arr[-1]&gt;sum(arr[:-1]):
+    print(arr[-1]-sum(arr[:-1]))
+else:
+    print(0)
