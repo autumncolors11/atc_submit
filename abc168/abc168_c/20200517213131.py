0d8c2afe60ad277957742b8bba4dcc365222159e
autumncolors11 <biomimetics500tour@gmail.com>
2020-05-18 00:00:10 +0900

    2020/05/18

---
+++ abc168/abc168_c/20200517213131.py
@@ -0,0 +1,43 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log,sin,cos,radians,tan
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+a,b,h,m=mii()
+h%=12
+
+hun = [b*sin(radians(360*m/60)),b*cos(radians(360*m/60))]
+
+zi=[a*sin(radians(360*h/12 + 360*m/60/12)),a*cos(radians(360*h/12+ 360*m/60/12))]
+
+ans = ((hun[0]-zi[0])**2 + (hun[1]-zi[1])**2)**0.5
+#print(hun,zi)
+print(ans)
+
+
