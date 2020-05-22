a010c78d354688f6da22500c40f478d815faa5d3
autumncolors11 <biomimetics500tour@gmail.com>
2020-05-22 23:00:06 +0900

    2020/05/22

---
+++ tenka1-2019-beginner/tenka1_2019_c/20200522153153.py
@@ -0,0 +1,61 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+n=ii()
+s=list(ss())
+
+mat=Counter(s)
+black=mat["#"]
+white=n-black
+
+ans=white
+
+if black==0 or white==0:
+    print(0)
+    exit()
+
+
+tmptowhite=0
+tmptoblack=white
+for i in range(n):
+    if s[i]=="#":
+        tmptowhite+=1
+
+    else:
+
+        tmptoblack-=1
+    
+    ans=min(tmptowhite+tmptoblack,ans)
+
+    
+print(ans)
+
+
