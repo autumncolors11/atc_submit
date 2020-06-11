4aaaff39d4c88e110c2eda0f225ba7578c50a966
autumncolors11 <biomimetics500tour@gmail.com>
2020-06-11 20:00:07 +0900

    2020/06/11

---
+++ agc032/agc032_a/20200611022059.py
@@ -0,0 +1,53 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+n=ii()
+arr=limii()
+ans=[]
+while len(arr)&gt;0:
+    chk=False
+    maxtmp=0
+    for i,j in enumerate(arr):
+        #print(i,j,i+1,arr[i])
+        if i+1==j:
+            maxtmp=j-1
+            chk=True
+    p=arr.pop(maxtmp)
+    ans.append(p)
+    if chk==False:
+        print(-1)
+        exit()
+    #print(ans)
+
+k=reversed(ans)
+for i,j in enumerate(k):
+    print(j)
+
