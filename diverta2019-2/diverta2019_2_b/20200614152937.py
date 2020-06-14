996b72028b26c8739610f817e9695f5c3f50355e
autumncolors11 <biomimetics500tour@gmail.com>
2020-06-14 20:00:10 +0900

    2020/06/14

---
+++ diverta2019-2/diverta2019_2_b/20200614152937.py
@@ -0,0 +1,65 @@
+import sys
+sys.setrecursionlimit(10**6)
+from math import floor,ceil,sqrt,factorial,log
+from heapq import heappop, heappush, heappushpop
+from collections import Counter,defaultdict,deque
+from itertools import accumulate,permutations,combinations,product,combinations_with_replacement
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+from operator import itemgetter
+from fractions import gcd
+mod = 10 ** 9 + 7
+inf = float('inf')
+ninf = -float('inf')
+'''
+#Pythonを用いる場合
+#numpy 1.18.0, scipy 1.4.1, scikit-learn 0.22, numba 0.47.0, networkx 2.4
+ 
+import numpy as np
+from scipy.sparse.csgraph import shortest_path, floyd_warshall, dijkstra, bellman_ford, johnson
+from scipy.sparse import csr_matrix
+'''
+ 
+#整数input
+def ii(): return int(sys.stdin.readline().rstrip()) #int(input())
+def mii(): return map(int,sys.stdin.readline().rstrip().split())
+def limii(): return list(mii()) #list(map(int,input().split()))
+def lin(n:int): return [ii() for _ in range(n)]
+def llint(n: int): return [limii() for _ in range(n)]
+#文字列input
+def ss(): return sys.stdin.readline().rstrip() #input()
+def mss(): return sys.stdin.readline().rstrip().split()
+def limss(): return list(mss()) #list(input().split())
+def lst(n:int): return [ss() for _ in range(n)]
+def llstr(n: int): return [limss() for _ in range(n)]
+
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+#本当に貪欲法か？ DP法では？？
+
+n=ii()
+
+arr=[list(mii()) for _ in range(n)]
+
+chk={}
+
+for i in arr:
+    for j in arr:
+        if i!=j:
+            p=i[0]-j[0]
+            q=i[1]-j[1]
+
+            if str(p)+","+str(q) in chk:
+                chk[str(p)+","+str(q)]+=1
+            else:
+                chk[str(p)+","+str(q)]=1
+
+places="w"
+num=0
+
+for i,j in chk.items():
+    if j&gt;num:
+        num=j
+        places=i
+
+print(n-num)
